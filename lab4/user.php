e509525db8b50e4ffdab41d938f6fb88ee6afbbc
yaranefaa <nefaayara@gmail.com>
2020-10-02 13:03:43 +0300

    Update user.php

--- lab4/user.php
+++ lab4/user.php
@@ -8,12 +8,12 @@
                 <title>$name</title>
              </head>
              <body>
-             <form method=\"POST\" action=\"./createhtml.php\">
+             <form method=\"POST\" action=\"./createhtml.php\"  enctype=\"multipart/form-data\">
              <fieldset>
                  <legend>Submit</legend>
                  Introduction <input name=\"intro\" type=\"textarea\">
                  <input type=\"file\" id=\"myFile\" name=\"filename\">
-                 <input type=\"submit\">
+                 <input type=\"submit\" value=\"Upload\">
  
              </fieldset>
          </form>
